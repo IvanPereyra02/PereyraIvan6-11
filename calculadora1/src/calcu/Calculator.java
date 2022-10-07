c28d982ade28137fa9150711f7eda0bb2e603d56
IvanPereyra02 <84509887+IvanPereyra02@users.noreply.github.com>
2022-10-06 22:15:44 -0300

    cuadratica

--- calculadora1/src/calcu/Calculator.java
+++ calculadora1/src/calcu/Calculator.java
@@ -16,4 +16,8 @@ public class Calculator {
     public int divicion (int numero1, int numero2){
         return numero1 / numero2;
     }
+    public double [] cuadratica (int numero1, int numero2, int numero3){
+        return (int) (- numero2  +- Math.sqrt(numero2 ^ 2 - 4*numero1*numero3) / 2 * numero1);
+
+    }
 }
