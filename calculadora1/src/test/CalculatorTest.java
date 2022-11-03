aeabc68e547e96793e090e45cdfdf6a6a494e415
IvanPereyra02 <84509887+IvanPereyra02@users.noreply.github.com>
2022-11-03 11:03:04 -0300

    cambios al test

--- calculadora1/src/test/CalculatorTest.java
+++ calculadora1/src/test/CalculatorTest.java
@@ -11,26 +11,33 @@ public class CalculatorTest{
  @Test
     public void sumaTest(){
      assertEquals(calculator.suma(1,2),3);
+     assertNotEquals(calculator.suma(7, 7),10);
 
  }
  @Test
   public void restaTest(){
   assertEquals(calculator.resta(2,1),1);
+  assertNotEquals(calculator.resta(7, 7),2);
 
  }
  @Test
  public void multiplicacionTest(){
   assertEquals(calculator.multiplicacion(3,3),9);
+  assertNotEquals(calculator.multiplicacion(7, 7),45);
 
  }
  @Test
- public void divicionTest(){
-  assertEquals(calculator.divicion(4,2),2);
+ public void divisionTest(){
+  assertEquals(calculator.division(4,2),2);
+  assertNotEquals(calculator.division(80, 7),45);
+
 
  }
  @Test
  public void cuadraticaTest(){
   assertEquals(calculator.cuadratica(2,14,20),-2,-5);
+  assertNotEquals(calculator.cuadratica(2,4,6),21,5);
+
 }
 
 }
