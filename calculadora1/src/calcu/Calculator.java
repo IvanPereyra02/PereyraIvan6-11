49ddd564175ba123fb1f15ab5a264d42587f02be
IvanPereyra02 <84509887+IvanPereyra02@users.noreply.github.com>
2022-11-03 10:48:32 -0300

    Update Calculator.java

--- calculadora1/src/calcu/Calculator.java
+++ calculadora1/src/calcu/Calculator.java
@@ -16,8 +16,21 @@ public class Calculator {
     public int divicion (int numero1, int numero2){
         return numero1 / numero2;
     }
-    public double [] cuadratica (int numero1, int numero2, int numero3){
-        return (int) (- numero2  +- Math.sqrt(numero2 ^ 2 - 4*numero1*numero3) / 2 * numero1);
+       float [] Cuadratica (float numero1, float numero2, float numero3){
+     float cuadrado = numero2 * numero2;
+     float raiz = (float) Math.sqrt( cuadrado-4* numero1* numero3);
+     float cuenta =(float) raiz;
+     float verificacion = cuenta;
+     if (verificacion% 2 == 0);{
+         float F1 = ((-numero2)+cuenta/(2*numero1));
+         float F2 = ((-numero2)-cuenta/(2*numero1));
+         float [] Resultado ={F1 , F2};
+
+         return  Resultado;
+
+        }
+    }
+}
 
     }
 }
