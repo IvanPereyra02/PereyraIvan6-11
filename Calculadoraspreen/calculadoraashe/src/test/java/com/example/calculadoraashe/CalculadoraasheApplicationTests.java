f5de401262f84447e3e9cc10f3cdfc798932cecb
IvanPereyra02 <84509887+IvanPereyra02@users.noreply.github.com>
2022-11-07 19:25:15 -0300

    calculadoraashei

---
+++ Calculadoraspreen/calculadoraashe/src/test/java/com/example/calculadoraashe/CalculadoraasheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadoraashe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraasheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
